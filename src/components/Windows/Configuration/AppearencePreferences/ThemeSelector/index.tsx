cd3c90fc56ae235aefbfabf8f498e3b325998ce8
Pablo Tilli <tillipablo@gmail.com>
2024-09-06 17:33:58 -0300

    style: update theme selector styles for improved UI/UX

--- src/components/Windows/Configuration/AppearencePreferences/ThemeSelector/index.tsx
+++ src/components/Windows/Configuration/AppearencePreferences/ThemeSelector/index.tsx
@@ -12,6 +12,7 @@ const Container = styled.div<{
   border-radius: 7px;
 
   border: 1px solid gray;
+  width: 100%;
 `;
 
 const TitleBarButtons = styled.div`
@@ -29,6 +30,18 @@ const TitleBarButton = styled.div`
   border-radius: 50%;
 `;
 
+const SelectorContainer = styled.div`
+  border: 3px solid #b1919129;
+  height: 130px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f3c9c929;
+
+  padding: 10px;
+`;
+
 export default function ThemeSelector({
   onClick,
   tema,
@@ -37,63 +50,66 @@ export default function ThemeSelector({
   tema: Theme;
 }) {
   return (
-    <Container onClick={onClick} tema={tema}>
-      <div
-        style={{
-          backgroundColor: tema.titleBarBackgroundColor,
-          height: '25px',
-          width: '100%',
-          position: 'relative',
-          borderTopLeftRadius: '5px',
-          border: '1px solid gray',
-        }}
-      >
-        <TitleBarButtons>
-          <TitleBarButton color="gray" />
-          <TitleBarButton color="gray" />
-          <TitleBarButton color="gray" />
-        </TitleBarButtons>
-
+    <SelectorContainer>
+      <Container onClick={onClick} tema={tema}>
         <div
           style={{
-            backgroundColor: tema.secondaryBackgroundColor,
-            height: '83px',
-            width: '16%',
-            position: 'absolute',
-            top: '25px',
-            left: 0,
-            borderBottomLeftRadius: '5px',
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: tema.mainBackgroundColor,
-            height: '83px',
-            width: '35%',
-            position: 'absolute',
-            top: '25px',
-            left: '16%',
-            border: '1px solid gray',
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: tema.titleBarActiveBackgroundColor,
+            backgroundColor: tema.titleBarBackgroundColor,
             height: '25px',
-            width: '85%',
-            position: 'absolute',
-            top: '15px',
-            right: 0,
+            width: '100%',
+            position: 'relative',
             borderTopLeftRadius: '5px',
+            border: '1px solid gray',
           }}
         >
           <TitleBarButtons>
-            <TitleBarButton color="#FF5F57" />
-            <TitleBarButton color="#FFBD2E" />
-            <TitleBarButton color="#28C940" />
+            <TitleBarButton color="gray" />
+            <TitleBarButton color="gray" />
+            <TitleBarButton color="gray" />
           </TitleBarButtons>
+
+          <div
+            style={{
+              backgroundColor: tema.secondaryBackgroundColor,
+              height: '83px',
+              width: '16%',
+              position: 'absolute',
+              top: '25px',
+              left: 0,
+              borderBottomLeftRadius: '5px',
+              borderBottom: '1px solid #999191',
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundColor: tema.mainBackgroundColor,
+              height: '83px',
+              width: '35%',
+              position: 'absolute',
+              top: '25px',
+              left: '16%',
+              border: '1px solid gray',
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundColor: tema.titleBarActiveBackgroundColor,
+              height: '25px',
+              width: '85%',
+              position: 'absolute',
+              top: '15px',
+              right: 0,
+              borderTopLeftRadius: '5px',
+            }}
+          >
+            <TitleBarButtons>
+              <TitleBarButton color="#FF5F57" />
+              <TitleBarButton color="#FFBD2E" />
+              <TitleBarButton color="#28C940" />
+            </TitleBarButtons>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </SelectorContainer>
   );
 }
